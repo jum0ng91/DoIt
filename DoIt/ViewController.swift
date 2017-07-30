d82e08a704fe7c99440f53b70e931e2111deaf68
GREED <greed@GREEDs-MacBook-Pro.local>
2017-07-29 20:56:59 -0700

    Set up Task class, create makeTasks function that puts tasks into an array, fill up cells with names in the array

--- DoIt/ViewController.swift
+++ DoIt/ViewController.swift
@@ -8,18 +8,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var tasks:[Task] = []; // Make an empty array called task
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tasks = makeTasks(); // Call the function makeTasks to create an array of tasks
+        tableView.dataSource = self;
+        tableView.delegate = self;
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count; // Set the number of rows to be the number of items in tasks array
     }
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell();
+        let task = tasks[indexPath.row]; // Fill cells with items in tasks
+        if(task.important) {
+            cell.textLabel?.text = "!\(task.name)"; // If task is important, add ! in front of it
+        } else {
+            cell.textLabel?.text = task.name; // Otherwise default to name
+        }
+        return cell;
+    }
+    
+    // Make function to create array
+    func makeTasks() -> [Task] {
+        let task1 = Task()
+        task1.name = "Walk the dog";
+        task1.important = false;
+        
+        let task2 = Task()
+        task2.name = "Buy Cheese";
+        task2.important = true;
+        
+        let task3 = Task()
+        task3.name = "Mow the lawn";
+        task3.important = false;
+        
+        return [task1, task2, task3];
+    }
 
 }
 
